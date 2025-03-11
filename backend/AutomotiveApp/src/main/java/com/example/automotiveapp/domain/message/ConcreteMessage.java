86fbf5d25388f6172223607f77b83adfccfd2916
Grzegorz <grzesio.szymanek@gmail.com>
2025-03-11 11:52:08 +0100

    refactor: remove redundant fields in the concrete message class

--- backend/AutomotiveApp/src/main/java/com/example/automotiveapp/domain/message/ConcreteMessage.java
+++ backend/AutomotiveApp/src/main/java/com/example/automotiveapp/domain/message/ConcreteMessage.java
@@ -1,29 +1,12 @@
 package com.example.automotiveapp.domain.message;
 
-import com.example.automotiveapp.domain.Channel;
-import com.example.automotiveapp.domain.User.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 public class ConcreteMessage extends Message {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne
-    @JoinColumn(name = "sender_id")
-    private User sender;
-    @ManyToOne
-    @JoinColumn(name = "receiver_id")
-    private User receiver;
-    @ManyToOne
-    @JoinColumn(name = "channel_id")
-    private Channel channel;
-    private String message;
-    private LocalDateTime createdAt;
+
 }
